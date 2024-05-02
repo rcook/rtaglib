a9c749601e0acedb7f512940421ab7ac0659b30d
Richard Cook <rcook@rcook.org>
2024-05-02 09:51:48 -0700

    Simplify dictionary usage

--- tagtagtag/scan.py
+++ tagtagtag/scan.py
@@ -17,10 +17,21 @@ _INCLUDE_EXTS = {
 }
 
 
+class DictPlus(dict):
+    _MISSING = object()
+
+    def get_or_add(self, key, func):
+        value = self.get(key, self.__class__._MISSING)
+        if value is self.__class__._MISSING:
+            value = func()
+            self[key] = value
+        return value
+
+
 @dataclass
 class ExtInfo:
     count: int
-    tag_infos: dict
+    tag_infos: DictPlus
 
 
 @dataclass
@@ -29,23 +40,21 @@ class TagInfo:
 
 
 def do_scan(ctx, dir):
-    ext_infos = {}
+    ext_infos = DictPlus()
     for p in walk_dir(dir=dir, include_exts=_INCLUDE_EXTS, ignore_dirs=_IGNORE_DIRS):
         ext = p.suffix.lower()
-        ext_info = ext_infos.get(ext)
-        if ext_info is None:
-            ext_info = ExtInfo(count=0, tag_infos={})
-            ext_infos[ext] = ext_info
+        ext_info = ext_infos.get_or_add(
+            ext,
+            lambda: ExtInfo(count=0, tag_infos=DictPlus()))
 
         ext_info.count += 1
 
         m = mutagen.File(p)
         if m.tags is not None:
             for k in m.tags.keys():
-                tag_info = ext_info.tag_infos.get(k)
-                if tag_info is None:
-                    tag_info = TagInfo(count=0)
-                    ext_info.tag_infos[k] = tag_info
+                tag_info = ext_info.tag_infos.get_or_add(
+                    k,
+                    lambda: TagInfo(count=0))
                 tag_info.count += 1
 
     for ext in sorted(ext_infos.keys()):
