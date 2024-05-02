95e42a212e74c7ffdcc27dda0a12d55171e4733a
Richard Cook <rcook@rcook.org>
2024-05-02 14:03:10 -0700

    Extract track disc

--- tagtagtag/inferred_info.py
+++ tagtagtag/inferred_info.py
@@ -13,27 +13,37 @@ class InferredInfo:
     album_safe_title: str
     track_title: str
     track_safe_title: str
+    track_disc: int
     track_number: int
 
     _FILE_NAME_RE = re.compile("^(?P<digits>\\d+)(?P<rest>.+)$")
 
     @classmethod
     def parse(cls, rel_path):
+        def strip_track_disc_number(s):
+            def strip_number(s):
+                m = cls._FILE_NAME_RE.match(s)
+                if m is None:
+                    return None, s
+                else:
+                    return int(m.group("digits")), m.group("rest").lstrip("_-. ")
+
+            number0, track_safe_title = strip_number(base_file_name)
+            number1, track_safe_title = strip_number(track_safe_title)
+            if number1 is None:
+                return None, number0, track_safe_title
+            else:
+                return number0, number1, track_safe_title
+
         parts = rel_path.parts
         if len(parts) < 3:
             raise ReportableError(
                 f"File path \"{rel_path}\" does not match expected structure")
 
         artist_safe_title, album_safe_title, file_name = parts[-3:]
-
         base_file_name, _ = os.path.splitext(file_name)
-        m = cls._FILE_NAME_RE.match(base_file_name)
-        if m is None:
-            track_number = None
-            track_safe_title = base_file_name
-        else:
-            track_number = int(m.group("digits"))
-            track_safe_title = m.group("rest").lstrip("_").lstrip(" ")
+        track_disc, track_number, track_safe_title = \
+            strip_track_disc_number(base_file_name)
 
         return cls(
             artist_title=humanize_str(artist_safe_title),
@@ -42,4 +52,5 @@ class InferredInfo:
             album_safe_title=make_safe_str(album_safe_title),
             track_title=humanize_str(track_safe_title),
             track_safe_title=make_safe_str(track_safe_title),
+            track_disc=track_disc,
             track_number=track_number)
