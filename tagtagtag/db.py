e5e108bbc54ff267e48dfad49791e9af944cf959
Richard Cook <rcook@rcook.org>
2024-05-02 00:01:54 -0700

    Create artist

--- tagtagtag/db.py
+++ tagtagtag/db.py
@@ -1,11 +1,14 @@
+from dataclasses import dataclass
 from pathlib import Path
-# from tagtagtag.album import Album
-# from tagtagtag.artist import Artist
+from tagtagtag.album import Album
+from tagtagtag.artist import Artist
+from tagtagtag.error import ReportableError
 from tagtagtag.fs import walk_dir
-# from tagtagtag.track import Track
+from tagtagtag.track import Track
 from tagtagtag.metadata import Metadata
 from tagtagtag.metadata_db import MetadataDB
 import os
+import re
 
 
 _IGNORE_DIRS = {
@@ -28,6 +31,49 @@ _MUSICBRAINZ_ATTRS = [
     ("musicbrainz_album_id", "MusicBrainz album ID"),
     ("musicbrainz_track_id", "MusicBrainz track ID")
 ]
+
+
+@dataclass(frozen=True)
+class InferredInfo:
+    title: str
+    title_fs: str
+    number: int
+    artist: str
+    artist_fs: str
+    album: str
+    album_fs: str
+
+    _FILE_NAME_RE = re.compile("^(?P<digits>\d+)(?P<rest>.+)$")
+
+    @classmethod
+    def parse(cls, rel_path):
+        def humanize(s):
+            return s.replace("_", " ").strip()
+
+        parts = rel_path.parts
+        if len(parts) != 3:
+            raise ReportableError(
+                f"File path \"{rel_path}\" does not match expected structure")
+
+        artist_fs, album_fs, file_name = parts
+
+        name, _ = os.path.splitext(file_name)
+        m = cls._FILE_NAME_RE.match(name)
+        if m is None:
+            number = None
+            rest = name
+        else:
+            number = int(m.group("digits"))
+            rest = m.group("rest")
+
+        return cls(
+            title=humanize(rest),
+            title_fs=file_name,
+            number=number,
+            artist=humanize(artist_fs),
+            artist_fs=artist_fs,
+            album=humanize(album_fs),
+            album_fs=album_fs)
 
 
 def do_db(ctx, data_dir):
@@ -81,13 +127,28 @@ def do_db(ctx, data_dir):
             if m.musicbrainz_track_id is not None:
                 continue
 
-            if m.title is None:
-                print(f"File: {p.relative_to(d)}")
+            rel_path = p.relative_to(d)
+            inferred = InferredInfo.parse(rel_path)
+
+            artist_name = inferred.artist if m.artist is None else m.artist
+            assert artist_name is not None and len(artist_name) > 0
+
+            album_name = inferred.artist if m.album is None else m.album
+            assert album_name is not None and len(album_name) > 0
+
+            title = inferred.title if m.title is None else m.title
+            assert title is not None and len(title) > 0
+
+            number = inferred.number if m.number is None else m.number
+
+            artist = Artist.query(db=db, name=artist_name, default=None)
+            if artist is None:
+                artist = Artist.create(
+                    db=db,
+                    name=artist_name,
+                    fs_name=inferred.artist_fs)
+                ctx.log_info(f"New artist: {artist.name} ({artist.uuid})")
             else:
-                print(f"Title: {m.title}")
-            print(f"Number: {m.number}")
-            print(f"Artist: {m.artist}")
-            print(f"Album: {m.album}")
-            print("-----")
+                ctx.log_info(f"Existing artist: {artist.name} ({artist.uuid})")
 
     ctx.log_debug("do_db end")
