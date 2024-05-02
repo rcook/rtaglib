2d6fee08e4072a923bb4b2f084c287a43b473344
Richard Cook <rcook@rcook.org>
2024-05-02 15:00:39 -0700

    Use metaclass to generate logging methods

--- tagtagtag/context.py
+++ tagtagtag/context.py
@@ -1,32 +1,29 @@
-from functools import cache
+from functools import cache, partialmethod
 import inspect
 import logging
 
 
-class Context:
-    _LOG_METHOD_PREFIX = "log_"
-    _LOG_LEVEL_NAMES = set(
-        map(
-            lambda m: m.lower(),
-            logging.getLevelNamesMapping().keys() - {"NOTSET", "WARN"}))
+class ContextMeta(type):
+    def __new__(cls, name, bases, dct):
+        t = super().__new__(cls, name, bases, dct)
 
+        for log_level in logging.getLevelNamesMapping().keys() - {"NOTSET", "WARN"}:
+            def log(self, log_level, *args, **kwargs):
+                frame = inspect.stack()[1]
+                module = inspect.getmodule(frame[0])
+                logger = self._logger(module.__name__)
+                method = getattr(logger, log_level)
+                return method(*args, **kwargs)
+
+            log_level = log_level.lower()
+            setattr(t, f"log_{log_level}", partialmethod(log, log_level))
+
+        return t
+
+
+class Context(metaclass=ContextMeta):
     def __init__(self, log_level=logging.DEBUG):
         self._log_level = log_level
-
-    def __getattr__(self, name):
-        if name.startswith(self.__class__._LOG_METHOD_PREFIX):
-            log_level = name[len(self.__class__._LOG_METHOD_PREFIX):]
-            if log_level in self._LOG_LEVEL_NAMES:
-                def log(*args, **kwargs):
-                    frame = inspect.stack()[1]
-                    module = inspect.getmodule(frame[0])
-                    logger = self._logger(module.__name__)
-                    method = getattr(logger, log_level)
-                    return method(*args, **kwargs)
-                return log
-        raise AttributeError(
-            "Undefined attribute "
-            f"{self.__class__.__name__}.{name}")
 
     @cache
     def _logger(self, name):
