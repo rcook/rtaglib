ffc099916c5a57b6e8cf3cfd46f7470579093b2b
Richard Cook <rcook@rcook.org>
2024-05-05 15:25:10 -0700

    Simplify edit

--- rtag/edit.py
+++ rtag/edit.py
@@ -9,8 +9,8 @@ def do_edit_artist(ctx):
         artist = select_artist(db=db)
 
         result = edit_item(item=artist)
-        if result is None or not result:
-            return result
+        if result is None:
+            return
 
         result.update(db=db)
         ctx.log_info(f"Updated artist with ID {artist.id}")
@@ -21,12 +21,10 @@ def do_edit_album(ctx):
 
     with ctx.open_db() as db:
         album = select_album(db=db)
-        if album is None or not album:
-            return album
 
         result = edit_item(item=album)
-        if result is None or not result:
-            return result
+        if result is None:
+            return
 
         result.update(db=db)
         ctx.log_info(f"Updated album with ID {album.id}")
@@ -37,12 +35,10 @@ def do_edit_track(ctx):
 
     with ctx.open_db() as db:
         track = select_track(db=db)
-        if track is None or not track:
-            return track
 
         result = edit_item(item=track)
-        if result is None or not result:
-            return result
+        if result is None:
+            return
 
         result.update(db=db)
         ctx.log_info(f"Updated track with ID {result.id}")
@@ -53,15 +49,10 @@ def do_edit_album_tracks(ctx):
 
     with ctx.open_db() as db:
         album = select_album(db=db)
-        if album is None or not album:
-            return album
 
         tracks = list(Track.list(db=db, album_id=album.id))
         for track in tracks:
             result = edit_item(item=track)
-            if result is not None and not result:
-                return result
-
             if result is not None:
                 result.update(db=db)
                 ctx.log_info(f"Updated track with ID {result.id}")
