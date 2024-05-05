7a4f7992a7222a1b27ac9fcaec5c812387c48db5
Richard Cook <rcook@rcook.org>
2024-05-04 18:23:21 -0700

    More

--- rtag/retag.py
+++ rtag/retag.py
@@ -1,9 +1,13 @@
 from rtag.constants import MUSIC_IGNORE_DIRS, MUSIC_INCLUDE_EXTS
 from rtag.fs import walk_dir
+from rtag.metadata.metadata import Metadata
 
 
 def do_retag(ctx, dir):
-    print(dir)
     with ctx.open_db():
         for p in walk_dir(dir, include_exts=MUSIC_INCLUDE_EXTS, ignore_dirs=MUSIC_IGNORE_DIRS):
-            print(p)
+            m = Metadata.load(p)
+            if m.musicbrainz_track_id is not None:
+                continue
+
+            assert m.rcook_track_id is not None
