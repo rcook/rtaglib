ce1fa263d016025fc05c8f677fd41f41ba3859f6
Richard Cook <rcook@rcook.org>
2024-05-04 18:52:24 -0700

    Look up artist, album and track in database

--- rtag/retag.py
+++ rtag/retag.py
@@ -1,8 +1,11 @@
 from colorama import Fore
+from rtag.album import Album
+from rtag.artist import Artist
 from rtag.constants import MUSIC_IGNORE_DIRS, MUSIC_INCLUDE_EXTS
 from rtag.cprint import cprint
 from rtag.fs import walk_dir
 from rtag.metadata.metadata import Metadata
+from rtag.track import Track
 
 
 def move_file(source_path, target_path):
@@ -10,7 +13,7 @@ def move_file(source_path, target_path):
 
 
 def do_retag(ctx, input_dir, music_dir, misc_dir):
-    with ctx.open_db():
+    with ctx.open_db() as db:
         for p in walk_dir(input_dir, include_exts=MUSIC_INCLUDE_EXTS, ignore_dirs=MUSIC_IGNORE_DIRS):
             rel_path = p.relative_to(input_dir)
             display_path = "/".join(rel_path.parts)
@@ -24,3 +27,9 @@ def do_retag(ctx, input_dir, music_dir, misc_dir):
                 continue
 
             assert m.rcook_track_id is not None
+            artist = Artist.get_by_uuid(db=db, uuid=m.rcook_artist_id)
+            album = Album.get_by_uuid(db=db, uuid=m.rcook_album_id)
+            track = Track.get_by_uuid(db=db, uuid=m.rcook_track_id)
+            print(artist)
+            print(album)
+            print(track)
