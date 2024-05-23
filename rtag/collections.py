ca839d7716e8d8dcda7bf442b715e968bf69b023
Richard Cook <rcook@rcook.org>
2024-05-22 22:12:58 -0700

    Rearrange files

--- rtag/collections.py
+++
@@ -1,9 +0,0 @@
-class DictPlus(dict):
-    _MISSING = object()
-
-    def get_or_add(self, key, func):
-        value = self.get(key, self.__class__._MISSING)
-        if value is self.__class__._MISSING:
-            value = func(key)
-            self[key] = value
-        return value
--- rtag/entity.py
+++
@@ -1,6 +0,0 @@
-from abc import ABC, abstractmethod
-
-
-class Entity(ABC):
-    @abstractmethod
-    def create_schema(db): raise NotImplementedError()
